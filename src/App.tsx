e453c71fa7f156311580570ebc9d5152ced45575
JamesEHart <jhart27@dtechhs.org>
2025-04-08 09:15:40 -0700

    typewriter animation

--- src/App.tsx
+++ src/App.tsx
@@ -2,38 +2,38 @@ import './App.css'
 
 function App() {
 
-  const element = document.querySelector('.shuffle-text') as HTMLElement;
+  // const element = document.querySelector('.shuffle-text') as HTMLElement;
 
-  if (element) {
-    const targetText: string = element.dataset.text || '';
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*';
-    let iterations = 0;
+  // if (element) {
+  //   const targetText: string = element.dataset.text || '';
+  //   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*';
+  //   let iterations = 0;
 
-    const shuffleInterval = setInterval(() => {
-      const scrambled = targetText
-        .split('')
-        .map((_char, i) => {
-          if (i < Math.floor(iterations)) return targetText[i];
-          return chars.charAt(Math.floor(Math.random() * chars.length));
-        })
-        .join('');
+  //   const shuffleInterval = setInterval(() => {
+  //     const scrambled = targetText
+  //       .split('')
+  //       .map((_char, i) => {
+  //         if (i < Math.floor(iterations)) return targetText[i];
+  //         return chars.charAt(Math.floor(Math.random() * chars.length));
+  //       })
+  //       .join('');
 
-      element.textContent = scrambled;
+  //     element.textContent = scrambled;
 
-      iterations += 1 / 3;
-      if (iterations >= targetText.length) {
-        clearInterval(shuffleInterval);
-        element.textContent = targetText;
-      }
-    }, 50);
-  }
+  //     iterations += 1 / 3;
+  //     if (iterations >= targetText.length) {
+  //       clearInterval(shuffleInterval);
+  //       element.textContent = targetText;
+  //     }
+  //   }, 50);
+  // }
  // adaw
   return (
     <>
-      {/* <div className="typewriter">
-        <p>Hello, world!</p>
-      </div> */}
-      <div className="shuffle-text" data-text="Hello, everyone!"></div>
+      <div className="typewriter">
+        <p>This is a really interesting paragraph</p>
+      </div>
+      {/* <div className="shuffle-text" data-text="Hello, everyone!"></div> */}
     </>
   )
 }
